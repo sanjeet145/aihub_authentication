95b0c791b9fd900439a2b143c9ffb54fc9130413
sanjeet145 <sanjeetverma9572@gmail.com>
2025-08-25 22:22:38 +0530

    changes in requirement.txt

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$u&_vvf8q=4z3ekg%-%*b8g7w&a##5)!ib+!aihk31f*7#5w&y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
