aa0db3156e307a3d431d83063b721f889400f0e9
sanjeet145 <sanjeetverma9572@gmail.com>
2025-08-30 21:46:39 +0530

    updated cors policy

--- backend/settings.py
+++ backend/settings.py
@@ -63,12 +63,18 @@ MIDDLEWARE = [
     
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    os.getenv("FRONTEND_URL"),
-]
+CORS_ALLOW_ALL_ORIGINS=True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     os.getenv("FRONTEND_URL"),
+# ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "content-type",
+    "authorization",
+]
 
 ROOT_URLCONF = 'backend.urls'
 
